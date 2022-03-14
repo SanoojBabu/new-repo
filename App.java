2172b5a900da633450ba0a2c17edb3ff6a1ad4cc
SanoojBabu <101546784+SanoojBabu@users.noreply.github.com>
2022-03-14 12:56:25 +0530

    Some more code added

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App{
 	public static void main(String[] args){
 		System.out.println("Hello");
 		System.out.println("Welcome to Sanooj Vlog!!");
+		System.out.print("Some more code added");
 	}
 }
